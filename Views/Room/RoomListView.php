367153e38c406280f9435df897ca96ae4ec103b9
Marius Klint Hansen <mariusklint@gmail.com>
2024-08-31 11:47:44 +0200

    Fixed broken image when room doesn't have images

--- Views/Room/RoomListView.php
+++ Views/Room/RoomListView.php
@@ -4,6 +4,7 @@ namespace KlintDev\WPBooking\Views\Room;
 
 use KlintDev\WPBooking\Components\Container;
 use KlintDev\WPBooking\Components\Formatter;
+use KlintDev\WPBooking\DTO\Room\RoomGetRequest;
 use KlintDev\WPBooking\DTO\Room\RoomListRequest;
 use KlintDev\WPBooking\MenuHandler;
 use KlintDev\WPBooking\Scripts\ScriptCollection;
@@ -14,44 +15,42 @@ use KlintDev\WPBooking\Views\ContentDependencyType;
 use KlintDev\WPBooking\Views\PartialPage;
 use ReflectionException;
 
-class RoomListView extends PartialPage
-{
-    protected static RoomListView $instance;
+class RoomListView extends PartialPage {
+	protected static RoomListView $instance;
 
-    public static function getInstance(): object
-    {
-        if (!isset(static::$instance)) {
-            self::$instance = new self();
-        }
-        return static::$instance;
-    }
+	public static function getInstance(): object {
+		if ( ! isset( static::$instance ) ) {
+			self::$instance = new self();
+		}
 
-    protected const PREFIX = "room-list-";
-    protected const ADD_BTN_ID = self::PREFIX . "add";
+		return static::$instance;
+	}
+
+	protected const PREFIX = "room-list-";
+	protected const ADD_BTN_ID = self::PREFIX . "add";
 
 	/**
 	 * @throws ReflectionException
 	 */
-	public static function render(): string|false
-    {
-        ob_start();
+	public static function render(): string|false {
+		ob_start();
 
-        $rooms = RoomService::getRooms();
+		$rooms = RoomService::getRooms();
 
-        ?>
+		?>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-6">
 
-                    <?php
-                    echo Container::beginDashboardContainer();
-                    echo Container::header(
-                        "Lokaler",
-                        true,
-                        self::ADD_BTN_ID,
-                        "Tilføj"
-                    );
-                    ?>
+					<?php
+					echo Container::beginDashboardContainer();
+					echo Container::header(
+						"Lokaler",
+						true,
+						self::ADD_BTN_ID,
+						"Tilføj"
+					);
+					?>
                     <table class="table table-striped table-hover">
                         <thead>
                         <tr>
@@ -62,40 +61,41 @@ class RoomListView extends PartialPage
                         </tr>
                         </thead>
                         <tbody>
-                        <?php foreach ($rooms as $room) { ?>
+						<?php foreach ( $rooms as $room ) { ?>
                             <tr>
                                 <td>
-                                    <img
-                                            alt="room image"
-                                            style="max-height: 200px; max-width: 200px; object-fit: scale-down;"
-                                            src="<?= wp_get_attachment_image_url($room->getPropertyValue(RoomListRequest::IMAGE_POST_ID_INT)) ?>"
-                                    >
+									<?php if ( $room->getPropertyValue( RoomListRequest::IMAGE_POST_ID_INT ) !== null ) { ?>
+                                        <img
+                                                alt="room image"
+                                                style="max-height: 200px; max-width: 200px; object-fit: scale-down;"
+                                                src="<?= wp_get_attachment_image_url( $room->getPropertyValue( RoomListRequest::IMAGE_POST_ID_INT ) ) ?>"
+                                        >
+									<?php } ?>
                                 </td>
-                                <td><?= $room->getPropertyValue(RoomListRequest::NAME_STR) ?></td>
-                                <td><?= Formatter::maxLengthText($room->getPropertyValue(RoomListRequest::DESCRIPTION_STR), 100) ?></td>
+                                <td><?= $room->getPropertyValue( RoomListRequest::NAME_STR ) ?></td>
+                                <td><?= Formatter::maxLengthText( $room->getPropertyValue( RoomListRequest::DESCRIPTION_STR ), 100 ) ?></td>
                                 <td style="text-align: center; vertical-align: middle">
                                     <a
-                                            href="<?= MenuHandler::getInstance()->HiddenMenuRoomsEdit->getUrl(["id" => $room->getPropertyValue(RoomListRequest::ID_INT)]) ?>"
+                                            href="<?= MenuHandler::getInstance()->HiddenMenuRoomsEdit->getUrl( [ "id" => $room->getPropertyValue( RoomListRequest::ID_INT ) ] ) ?>"
                                             class="btn btn-outline-primary"
                                             style="border: 0">
                                         <span class="dashicons dashicons-edit-large"></span>
                                     </a>
                                 </td>
                             </tr>
-                        <?php } ?>
+						<?php } ?>
                         </tbody>
                     </table>
-                    <?= Container::endDashboardContainer(); ?>
+					<?= Container::endDashboardContainer(); ?>
                 </div>
             </div>
         </div>
 
-        <?php return ob_get_clean();
-    }
+		<?php return ob_get_clean();
+	}
 
-    protected static function inlineScript(): string|false
-    {
-        ob_start(); ?>
+	protected static function inlineScript(): string|false {
+		ob_start(); ?>
         <script>
             jQuery(() => {
                 jQuery("#<?= self::ADD_BTN_ID ?>")
@@ -104,21 +104,20 @@ class RoomListView extends PartialPage
                     });
             });
         </script>
-        <?php return ob_get_clean();
-    }
+		<?php return ob_get_clean();
+	}
 
-    public static function getRequiredContent(): array
-    {
-        return [
-            new ContentDependency(
-                ContentDependency::INLINE_SCRIPT_HANDLE,
-                self::inlineScript(),
-                ContentDependencyType::Script,
-                ContentDependencyLoadingStyle::InlineContent,
-                [
-                    ScriptCollection::JQUERY
-                ]
-            )
-        ];
-    }
+	public static function getRequiredContent(): array {
+		return [
+			new ContentDependency(
+				ContentDependency::INLINE_SCRIPT_HANDLE,
+				self::inlineScript(),
+				ContentDependencyType::Script,
+				ContentDependencyLoadingStyle::InlineContent,
+				[
+					ScriptCollection::JQUERY
+				]
+			)
+		];
+	}
 }
